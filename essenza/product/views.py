b88f3f2189c3f4b7f1fb3983ce21850656d43667
Celia <celiasuarezcoronel@gmail.com>
2025-11-12 22:18:37 +0100

    Funcionalidad de stock y formateo del código

--- essenza/product/views.py
+++ essenza/product/views.py
@@ -1,6 +1,39 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.shortcuts import redirect, render
 from django.views import View
+
+from .models import Product
+
 
 class DashboardView(View):
     def get(self, request):
-        return render(request, 'product/dashboard.html')
+        return render(request, "product/dashboard.html")
+
+
+class StockView(View):
+    def get(self, request):
+        # Carga y muestra todos los productos ordenados por nombre
+        products = Product.objects.all().order_by("name")
+        return render(request, "product/stock.html", {"products": products})
+
+    def post(self, request):
+        # Coge datos del formulario para actualizar stock
+        product_id = request.POST.get("product_id")
+        stock = request.POST.get("stock")
+
+        # Encuentra el producto por su ID
+        product = Product.objects.get(pk=product_id)
+        if not product:
+            messages.error(request, "Producto no encontrado.")
+            return redirect("stock")
+
+        # Actualiza el stock del producto
+        new_stock = int(stock)
+        product.stock = new_stock
+        product.save(update_fields=["stock"])
+        messages.success(
+            request, f"Stock de '{product.name}' actualizado a {new_stock}."
+        )
+
+        # Recarga la misma página
+        return redirect("stock")
--- essenza/user/models.py
+++ essenza/user/models.py
@@ -1,18 +1,20 @@
-from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.db import models
+
 
 class Role(models.TextChoices):
-    ADMIN = 'admin', 'Admin'
-    USER = 'user', 'User'
+    ADMIN = "admin", "Admin"
+    USER = "user", "User"
+
 
 class Usuario(AbstractUser):
-    photo = models.ImageField(upload_to='profile_pics/', null=True, blank=True)
+    photo = models.ImageField(upload_to="profile_pics/", null=True, blank=True)
     role = models.CharField(max_length=10, choices=Role.choices, default=Role.USER)
 
-    email = models.EmailField(unique=True) 
+    email = models.EmailField(unique=True)
 
-    USERNAME_FIELD = 'email'               
-    REQUIRED_FIELDS = ['username']     
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["username"]
 
     def __str__(self):
         return self.email
