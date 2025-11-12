082e92902a3084b48d0d76976a94fc9128e31672
FRANCISCO DE CASTRO <pacodecm@gmail.com>
2025-11-12 21:54:53 +0100

    Actualizado .gitignore y carpeta para imágenes de sampleo

--- essenza/load_samples.bat
+++ essenza/load_samples.bat
@@ -1,7 +1,7 @@
 @echo off
 REM ---------------------------------------------------------
 REM IMPORTANTE: Este archivo borra todos los datos de tu BD local (y la crea con los datos de sampleo).
-REM
+REM             Las imágenes de sampleo se copian a la carpeta 'media/'.
 REM             También instala las dependencias necesarias definidas en 'requirements.txt' (si aun no lo están).
 REM ---------------------------------------------------------
 
@@ -16,19 +16,28 @@ pip install -r requirements.txt && (
         python manage.py migrate --noinput && (
             
             echo.
-            echo --- Cargando datos de USER...
-            python manage.py loaddata user/sample/sample.json && (
-                
+            echo --- Copiando imagenes de sampleo a 'media/'...
+            REM XCOPY [origen] [destino] /E /I /Y
+            REM /E = Copia subdirectorios (incluso vacíos)
+            REM /I = Si el destino no existe, asume que es un directorio
+            REM /Y = Suprime la pregunta de "sobreescribir archivo"
+            XCOPY _sample_assets media /E /I /Y && (
+            
                 echo.
-                echo --- Cargando datos de PRODUCT...
-                python manage.py loaddata product/sample/sample.json && (
+                echo --- Cargando datos de USER...
+                python manage.py loaddata user/sample/sample.json && (
                     
                     echo.
-                    echo --- Cargando datos de ORDER...
-                    python manage.py loaddata order/sample/sample.json && (
+                    echo --- Cargando datos de PRODUCT...
+                    python manage.py loaddata product/sample/sample.json && (
                         
                         echo.
-                        echo --- !Proceso completado! La base de datos esta lista. ---
+                        echo --- Cargando datos de ORDER...
+                        python manage.py loaddata order/sample/sample.json && (
+                            
+                            echo.
+                            echo --- !Proceso completado! La base de datos esta lista. ---
+                        )
                     )
                 )
             )
