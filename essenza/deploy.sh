707fbc3cebbdcdbac60417aedb1a1be32bcdf2fe
Alejandro Mantecon Rodriguez <alemanrod@alum.us.es>
2025-11-17 17:37:49 +0100

    load_samples

--- essenza/deploy.sh
+++ essenza/deploy.sh
@@ -1,8 +1,2 @@
 pip install -r requirements.txt && \
-python manage.py makemigrations && \
-python manage.py flush --noinput && \
-python manage.py migrate --noinput && \
-python manage.py collectstatic --noinput && \
-python manage.py loaddata user/sample/sample.json && \
-python manage.py loaddata product/sample/sample.json && \
-python manage.py loaddata order/sample/sample.json
+load_samples.bat
--- essenza/load_samples.bat
+++ essenza/load_samples.bat
@@ -1,9 +1,5 @@
 @echo off
-REM ---------------------------------------------------------
-REM IMPORTANTE: Este archivo borra todos los datos de tu BD local (y la crea con los datos de sampleo).
-REM             Las imágenes de sampleo se copian a la carpeta 'media/'.
-REM             También instala las dependencias necesarias definidas en 'requirements.txt' (si aun no lo están).
-REM ---------------------------------------------------------
+
 
 echo --- Instalando dependencias (pip)...
 pip install -r requirements.txt && (
@@ -17,10 +13,6 @@ pip install -r requirements.txt && (
             
             echo.
             echo --- Copiando imagenes de sampleo a 'media/'...
-            REM XCOPY [origen] [destino] /E /I /Y
-            REM /E = Copia subdirectorios (incluso vacíos)
-            REM /I = Si el destino no existe, asume que es un directorio
-            REM /Y = Suprime la pregunta de "sobreescribir archivo"
             XCOPY _sample_assets media /E /I /Y && (
             
                 echo.
