4fb08bde3b3a4aabdc93dc4909082e74f0c940cd
Alejandro Mantecon Rodriguez <alemanrod@alum.us.es>
2025-11-17 17:33:02 +0100

    makemigrations

--- essenza/deploy.sh
+++ essenza/deploy.sh
@@ -1,4 +1,5 @@
 pip install -r requirements.txt && \
+python manage.py makemigrations && \
 python manage.py flush --noinput && \
 python manage.py migrate --noinput && \
 python manage.py collectstatic --noinput && \
