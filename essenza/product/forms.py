70ba32b4bb1c73fada929f58804c5ee6a3e51529
Alejandro Mantecon Rodriguez <alemanrod@alum.us.es>
2025-11-18 19:15:47 +0100

    hotfix

--- essenza/product/forms.py
+++ essenza/product/forms.py
@@ -4,6 +4,18 @@ from .models import Product
 
 
 class ProductForm(forms.ModelForm):
+    photo = forms.ImageField(
+        label="Foto (Opcional)", required=False, widget=forms.FileInput
+    )
+    remove_photo = forms.BooleanField(required=False, label="Eliminar foto actual")
+    stock = forms.IntegerField(label="Cantidad en stock", min_value=0, required=False)
+    is_active = forms.BooleanField(
+        required=False, label="Producto activo", initial=True
+    )
+    description = forms.CharField(
+        label="Descripción", widget=forms.Textarea(attrs={"rows": 4})
+    )
+
     class Meta:
         model = Product
         fields = [
