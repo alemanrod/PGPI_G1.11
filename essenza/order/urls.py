6d5c9aa6bcdd3352c15c4177c90c96483046bfe0
Javier Gea <javgeaber@alum.us.es>
2025-11-22 14:25:50 +0100

    Lista de pedidos arreglada, información de pedido y redirects correctos

--- essenza/order/urls.py
+++ essenza/order/urls.py
@@ -2,13 +2,15 @@ from django.urls import path
 from .views import (
     OrderListAdminView,
     OrderListUserView,
-    OrderTrackView
+    OrderTrackView,
+    OrderDetailView,
 )
 
 urlpatterns = [
     path("admin/listado/", OrderListAdminView.as_view(), name="order_list_admin"),
     path("mis-pedidos/", OrderListUserView.as_view(), name="order_list_user"),
     path("seguimiento/", OrderTrackView.as_view(), name="order_track"),
+    path("pedido/<int:pk>/", OrderDetailView.as_view(), name="order_detail"),
 
 ]
 
