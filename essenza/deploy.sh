d8b06beb82699103a776c3e544d78d7337dea831
Alejandro Mantecon Rodriguez <alemanrod@alum.us.es>
2025-11-23 14:09:35 +0100

    .sh

--- essenza/deploy.sh
+++ essenza/deploy.sh
@@ -1,7 +1,10 @@
 pip install -r requirements.txt && \
+mkdir -p media && \
+rsync -av --ignore-errors _sample_assets/ media/ && \
+python manage.py makemigrations && \
 python manage.py flush --noinput && \
 python manage.py migrate --noinput && \
-# python manage.py collectstatic --noinput && \
+python manage.py collectstatic --noinput && \
 python manage.py loaddata user/sample/sample.json && \
 python manage.py loaddata product/sample/sample.json && \
 python manage.py loaddata order/sample/sample.json
